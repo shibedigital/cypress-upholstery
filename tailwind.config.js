4b150bd933dd5e9429e1b743352121a669cfd495
Thomas Mclennan <tgmclennan@hotmail.com>
2021-02-08 01:29:04 +0000

    tailwind cli update

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: { enabled: true, content: ['./index.html'] },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
