f7b23f6384aa4de2550b91c44c0d067452ec1617
Exac <thomas@DESKTOP-UEJ6PVP.localdomain>
2021-02-07 18:47:49 -0800

    compile css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: { enabled: true, content: ['./index.html'] },
+  purge: { enabled: true, content: ['**/*.html', './index.html', 'index.html'] },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
